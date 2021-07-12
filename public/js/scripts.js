1dfb2709010ad3a84d8cb6fd275f6fcef47ae476
AngelGris76 <sebastian.smuraglia@gmail.com>
2021-07-12 19:43:14 -0300

    Some correction on aria atributes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,8 +3,6 @@ const mainMenu = document.getElementById("main-menu");
 
 const toggleMenu = () => {
   mainMenu.classList.toggle("main-menu--hide");
-  if (mainMenu.ariaHidden === "true") mainMenu.ariaHidden = "false";
-  else mainMenu.ariaHidden = "true";
 };
 
 menuButton.addEventListener("click", () => {
